6980a532bd6292abdd2205a464a5ba9268b5560c
Romesh Gamadikari <gamadikarir@gmail.com>
2025-03-25 22:51:28 +0530

    Update ContactUs.jsx

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -125,7 +125,7 @@ const ContactUs = () => {
                     d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
                   />
                 </svg>
-                1055 Arthur Ave, Elk Groot, 67, New Palms, South Carolina
+                Kandy, Sri Lanka
               </p>
               <p className="flex items-center text-gray-700">
                 <svg
@@ -142,7 +142,7 @@ const ContactUs = () => {
                     d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                   />
                 </svg>
-                +1 234 678 9108 99
+                +94 81 249 0001
               </p>
               <p className="flex items-center text-gray-700">
                 <svg
@@ -159,11 +159,11 @@ const ContactUs = () => {
                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                   />
                 </svg>
-                Contact@moralizer.com
+                hello@travigo.com
               </p>
             </div>
 
-            {/* Office Hours */}
+            
             <div className="mt-6">
               <h3 className="text-xl font-semibold text-blue-900 mb-2">
                 Office Hours
