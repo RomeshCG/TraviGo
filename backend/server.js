19e23b3bdb8dc9e6cb4d5f685cd1182b716e3018
Romesh Gamadikari <gamadikarir@gmail.com>
2025-04-03 19:01:42 +0530

    port

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const adminRoutes = require('./routes/api');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
